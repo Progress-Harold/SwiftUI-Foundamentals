aede8ec64fb110c2810c50b1556eda7f71694b0a
Lee Davis <harold_davis@icloud.com>
2021-08-12 20:10:14 -0700

    Barcode Scanner - Intro & UISetup

---
+++ SwiftUI-BarcodeScanner/SwiftUI-BarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  SwiftUI-BarcodeScanner
+//
+//  Created by Lee Davis on 8/12/21.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Rectangle()
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: 300)
+                
+                Spacer().frame(height: 60)
+                
+                Label("Scanned Barcode", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                Text("Not Yet Scanned")
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .padding()
+            }
+            .navigationTitle("Barcode Scanner")
+        }
+    }
+}
+
+struct BarcodeScannerView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarcodeScannerView()
+    }
+}
---
+++ SwiftUI-BarcodeScanner/SwiftUI-BarcodeScanner/SwiftUI_BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BarcodeScannerApp.swift
+//  SwiftUI-BarcodeScanner
+//
+//  Created by Lee Davis on 8/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
